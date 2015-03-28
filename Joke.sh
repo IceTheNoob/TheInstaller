fa6772f93f7f23ea1c2068793a46eeaf11b85f3a
IceTheNoob <emilioarner10@outlook.com>
2015-03-28 14:45:50 -0500

    Create Joke.sh
    Play A joke with your friends inculding rickroll and the youareanidiot video
    Also The youareanidiot is safe Play a joke on your friends!

---
+++ Joke.sh
@@ -0,0 +1,41 @@
+export PS1="\[$(tput setaf 4)\]\u@\h:\w $ \[$(tput sgr0)\]"
+sudo apt-get install gnustep-gui-runtime
+echo "WARNING IP TRACE DETECTED"
+say "warning internet protocol trace detected"
+say "please wait while we are scanning"
+speaker-test -t sine -f 232738 -l 1
+echo "Scanning."
+say "scanning."
+sleep 2
+echo "Scanning.."
+say "scanning.."
+sleep 2
+echo "Scanning..."
+say "scanning..."
+say "WARNING"
+say "WARNING"
+say "WARNING"
+say "WARNING"
+say "WARNING"
+sleep 2
+echo "Threat Detected! Removing now"
+say "Threat Detected! REmoving now"
+sleep 2
+sleep 4
+echo "4 threats detected on your internet protcol"
+say "4 Threats detected on your internet protocol"
+sleep 3
+echo "Fixing it"
+say "fixing it"
+apt-get update
+echo "Oops We couldn't remove all threats"
+say "oops we couldn't remove all the threats"
+echo "11 More Threats found"
+say "11 more threats found"
+say "WARNING REMOVE NOW BEFORE THE THREATS ARE VERY HIGH RISK"
+x-www-browser http://youareanidiot.org/
+x-www-browser http://phonehelpfree.tk/
+say "Lel Haha You Got Fooled "
+say "LolSend this to your friends"
+say "Yes"
+say "We did not do anything to your system  smiley face"
