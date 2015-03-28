aae390befe797ca69617c71f6cdd610526d833cd
IceTheNoob <emilioarner10@outlook.com>
2015-03-27 23:31:06 -0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,1 +1,12 @@
+echo "welcome to the every thing you need to start out with when installing the ubuntu on chronos RUN AS ROOT"
+echo "wait 4 seconds for bundle to install"
+echo "Did you install linux? to tired to install all of this? well use the bundle"
+echo "Note: Some might not work"
+sleep 4
+apt-get install software-center
+apt-get install firefox
+apt-get install adobe-flashplugin
+apt-get update
+apt-get upgrade
+apt-get nano
 
